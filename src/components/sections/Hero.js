0005c98ca8f68b6ff79a17c3e29453f3c826ca8c
suryasu <surya@caltech.edu>
2021-05-27 19:08:03 -0700

    Added first storyboard

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,13 +61,13 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Restaurant <span className="text-color-primary">success</span> and <span className="text-color-primary">survival</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Visualizations of Yelp's Academic Dataset
                 </p>
-              <div className="reveal-from-bottom" data-reveal-delay="600">
+              {/* <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
                     Get started
@@ -76,10 +76,10 @@ const Hero = ({
                     View on Github
                     </Button>
                 </ButtonGroup>
-              </div>
+              </div> */}
             </div>
           </div>
-          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
+          {/* <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
               data-video="https://player.vimeo.com/video/174002812"
               href="#0"
@@ -99,9 +99,9 @@ const Hero = ({
             show={videoModalActive}
             handleClose={closeModal}
             video="https://player.vimeo.com/video/174002812"
-            videoTag="iframe" />
+            videoTag="iframe" /> */}
         </div>
-      </div>
+      </div> 
     </section>
   );
 }
