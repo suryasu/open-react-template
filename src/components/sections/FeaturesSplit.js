65561cf0a8e832606b35f93fb156e7e21952368b
suryasu <surya@caltech.edu>
2021-05-29 21:29:08 -0400

    debugging

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -39,7 +39,7 @@ class FeaturesSplit extends React.Component {
 
   render() {
     return (
-      <section className={classNames('has-bottom-divider', "reveal-from-bottom")} data-reveal-delay="400">
+      <section className={classNames('has-top-divider', "reveal-from-bottom")} data-reveal-delay="400">
         <div className={classNames('container', 'center-content','feature-split flex-container')}>
           <h3>
             Are certain states more suited for restaurants than other states?
@@ -47,7 +47,7 @@ class FeaturesSplit extends React.Component {
           <p>
             Let's take a look at the states included in Yelp's dataset.
           </p>
-          <div className='feature-split tableau'>
+          <div className={classNames=('feature-split tableau', 'content-sm')}>
             <div ref={(div) => { this.vizContainer1 = div }}></div>
           </div>
           <p>
